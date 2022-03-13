2750baf96b582aa9533f68315e5368420dbf7153
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-03-13 19:20:20 -0300

    Create Principal.java

---
+++ empresa/empresa/Principal.java
@@ -0,0 +1,13 @@
+package empresa;
+
+public class Principal {
+  public static void main(String[] args){
+    int num=10;
+    float peso;
+    String nome = "Mario";
+    int tamalho = nome.length();
+    nome = "Super " + nome;
+    System.out.println("Hello world");
+    System.out.println(nome); //o print ln printa a linha. Ja o print, printa uma coisa do lado da outra 
+  }
+}
