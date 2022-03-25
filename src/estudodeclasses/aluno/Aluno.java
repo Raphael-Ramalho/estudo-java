a21cefb2a393d6820e3b6e50f027532d2994367e
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-03-24 22:40:49 -0300

    update

--- src/estudodeclasses/aluno/Aluno.java
+++ src/estudodeclasses/aluno/Aluno.java
@@ -4,10 +4,14 @@ public class Aluno { //a classe dita como será a estrutura dos objetos
   String nome;
   String cpf;
 
-  Aluno(int pMatricula, String pNome, String pCpf){ //construtor para automatizar a população de dados na criação de novas instâncias
-    matricula = pMatricula;
-    nome = pNome;
-    cpf = pCpf;
+  Aluno(){
+    System.out.println("Aluno vazio criado");
+  }
+
+  Aluno(int matricula, String nome, String cpf){ //construtor para automatizar a população de dados na criação de novas instâncias
+    this.matricula = matricula; //o this faz referência à estância atual da classe
+    this.nome = nome;
+    this.cpf = cpf;
   }
 
   void info(){ //criação de um metodo para a classe aluno
