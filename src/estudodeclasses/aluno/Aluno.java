e35b311d118768220a490ff8098417158994e180
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-03-25 16:50:50 -0300

    update

--- src/estudodeclasses/aluno/Aluno.java
+++ src/estudodeclasses/aluno/Aluno.java
@@ -1,20 +1,21 @@
 package estudodeclasses.aluno;
 public class Aluno { //a classe dita como será a estrutura dos objetos
-  int matricula;
+  private  int matricula;
   String nome;
   String cpf;
+  protected int notas[];
 
   Aluno(){
     System.out.println("Aluno vazio criado");
   }
 
-  Aluno(int matricula, String nome, String cpf){ //construtor para automatizar a população de dados na criação de novas instâncias
+  public Aluno(int matricula, String nome, String cpf){ //construtor para automatizar a população de dados na criação de novas instâncias
     this.matricula = matricula; //o this faz referência à estância atual da classe
     this.nome = nome;
     this.cpf = cpf;
   }
 
-  void info(){ //criação de um metodo para a classe aluno
+  public void info(){ //criação de um metodo para a classe aluno
     System.out.println("Matricula: " + matricula);
     System.out.println("Nome: " + nome);
     System.out.println("cpf: " + cpf);
---
+++ src/estudodeclasses/outro/Teste.java
@@ -0,0 +1,11 @@
+package estudodeclasses.outro;
+
+import estudodeclasses.aluno.Aluno;
+
+public class Teste {
+  public static void teste(String[] args) {
+    Aluno a = new Aluno(1001, "Super Mario", "111.222.333-4");
+
+    a.info();
+  }
+}
